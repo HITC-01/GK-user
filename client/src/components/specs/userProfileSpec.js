1336c29e2a3900b2b1d357bbe89a6e94079b5d4e
gkim795 <glory@g.com>
2018-11-01 00:12:32 -0700

    added userProfile testing

--- client/src/components/specs/userProfileSpec.js
+++ client/src/components/specs/userProfileSpec.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import UserProfile from '../userProfile.jsx';
+import { shallow } from '../../enzyme';
 
-const testUserData = {
-  id: 2,
-  pro: 'true',
-  isFollowing: 'false',
-  followers: 69,
-  trackCount: 28,
-  userName: 'Trevor Ankunding IV',
-  profilePhoto: 'https://s3.amazonaws.com/uifaces/faces/twitter/jjshaw14/128.jpg',
-  location: 'Davismouth, Montserrat',
-};
 
 describe('<UserProfile />', () => {
+  let props = {};
+  
+  beforeEach(()=>{
+    props = {
+      id: 2,
+      pro: 'true',
+      isFollowing: 'false',
+      followers: 69,
+      trackCount: 28,
+      userName: 'Trevor Ankunding IV',
+      profilePhoto: 'https://s3.amazonaws.com/uifaces/faces/twitter/jjshaw14/128.jpg',
+      location: 'Davismouth, Montserrat',
+    };
+  });
+    
   test('Should render correctly if no userData is passed', () => {
     const component = renderer.create(<UserProfile />);
     const tree = component.toJSON();
@@ -23,7 +29,7 @@ describe('<UserProfile />', () => {
 
 
   test('Should correctly render userdata', () => {
-    const component = renderer.create(<UserProfile userData={testUserData} />);
+    const component = renderer.create(<UserProfile userData={props} />);
     const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
@@ -34,5 +40,11 @@ describe('<UserProfile />', () => {
     const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  test('Should work with props', () => {
+    const component = shallow(<UserProfile {...props} />);
+    const propsOut = Array.from(Object.keys(component.props()));
+    expect(propsOut.length).toBe(2);
   });
 });
