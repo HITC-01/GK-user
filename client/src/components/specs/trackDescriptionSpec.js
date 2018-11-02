61c585c2920e754f5c87ac1b34edb33bbbf08101
gkim795 <glory@g.com>
2018-11-02 12:27:16 -0700

    added tests for track description

--- client/src/components/specs/trackDescriptionSpec.js
+++ client/src/components/specs/trackDescriptionSpec.js
@@ -1,7 +1,11 @@
+/* eslint-env jest */
+/* eslint react/jsx-filename-extension: 0 */
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 import TrackDescription from '../trackDescription.jsx';
 import HashTag from '../hashtag.jsx';
+import { shallow } from '../../enzyme';
 
 describe('<TrackDescription />', () => {
   test('Should render correctly', () => {
@@ -11,10 +15,33 @@ describe('<TrackDescription />', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  test('Should render correctly', () => {
-    const component = renderer.create(<TrackDescription />);
-    const tree = component.toJSON();
+  test('Should not render the HashTag Component if there are no hashes ', () => {
+    const props = {
+      description: 'Testing for the track description',
+      hashtags: '',
+      id: 80,
+      userId: 90,
+    };
+    const component = shallow(<TrackDescription songData={props} />);
 
-    expect(tree).toMatchSnapshot();
+    expect(component.find(HashTag).length).toBe(0);
+  });
+
+  test('Should render the HashTag Component if there are hashes ', () => {
+    const propsWithHash = {
+      description: 'Testing for the track description',
+      hashtags: 'Helllo',
+      id: 80,
+      userId: 90,
+    };
+    const component = shallow(<TrackDescription songData={propsWithHash} />);
+    expect(component.find(HashTag).length).toBe(1);
+  });
+
+  test('Should change class of show / hide based on state change', () => {
+    const component = shallow(<TrackDescription />);
+    expect(component.find('.utd-show').length).toBe(1);
+    component.setState({ isToggleOn: false });
+    expect(component.find('.utd-show').length).toBe(0);
   });
 });
