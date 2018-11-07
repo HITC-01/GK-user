b5853dffbe63671b51c6db94005a7cced8c72fb9
gkim795 <glory@g.com>
2018-11-06 16:35:23 -0800

    rebundled

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './components/page.jsx';
 
+window.Page = Page;
 ReactDOM.render(<Page />, document.getElementById('app'));
